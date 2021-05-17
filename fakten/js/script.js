3f13b4193e47e212db439917c2485dc3d4b59fe1
sodnor <84066118+sodnor@users.noreply.github.com>
2021-05-17 21:00:11 +0200

    Update script.js

--- fakten/js/script.js
+++ fakten/js/script.js
@@ -2,7 +2,6 @@ var speakButton = document.querySelector("#speak-button");
 var speakText = document.querySelector("#speak-text");
 var playerThese = document.querySelector("#audio-these");
 var playerJaNein = document.querySelector("#audio-janein");
-// var emojiFooter = document.querySelector("#emoji-btn");
 var currentThese = "Gefühlte Fakten ist der beste Podcast";
 var utter = new SpeechSynthesisUtterance();
 utter.lang = 'de-DE';
@@ -12,17 +11,9 @@ function rndStr(myArray) {
     return myArray[RandomValue];
 }
 
-/* function emojiShuffle() {
-    emojiFooter.innerHTML = rndStr(emojis);
-} */
-
 window.onload = function() {
     speakText.setAttribute("placeholder", rndStr(phThese))
 };
-
-/* emojiFooter.addEventListener('click', function() {
-    emojiShuffle();
-}) */
 
 speakButton.addEventListener('click', function() {
     if (speakText.value.trim() != '') {
@@ -32,17 +23,12 @@ speakButton.addEventListener('click', function() {
     }
     //Button Settings
     speakButton.disabled = true;
-    /* var intervalId = window.setInterval(function() {
-        emojiShuffle();
-    }, 400); */
     //set These audio source
     playerThese.setAttribute("src", rndStr(srcThese));
     //set Ja Nein audio source
     playerJaNein.setAttribute("src", rndStr(srcJaNein));
     //when JaNein is loaded, start function
     playerJaNein.oncanplay = function() {
-        clearInterval(intervalId);
-        speakButton.textContent = "🔊";
         //1 Thesen Einleitung
         playerThese.play();
         //when These is said, continue
@@ -63,49 +49,49 @@ speakButton.addEventListener('click', function() {
 });
 
 var srcThese = [
-    "source/thesen/14These1.mp3",
-    "source/thesen/14These2.mp3",
-    "source/thesen/14These3.mp3",
-    "source/thesen/15These1.mp3",
-    "source/thesen/15These2.mp3",
-    "source/thesen/15These3.mp3",
-    "source/thesen/16These1.mp3",
-    "source/thesen/16These2.mp3",
-    "source/thesen/16These3.mp3",
-    "source/thesen/17These1.mp3",
-    "source/thesen/17These2.mp3",
-    "source/thesen/17These3.mp3",
-    "source/thesen/18These1.mp3",
-    "source/thesen/18These2.mp3",
-    "source/thesen/18These3.mp3",
-    "source/thesen/20These1.mp3",
-    "source/thesen/20These2.mp3",
-    "source/thesen/20These3.mp3",
-    "source/thesen/21These1.mp3",
-    "source/thesen/21These2.mp3",
-    "source/thesen/21These3.mp3",
-    "source/thesen/22These1.mp3",
-    "source/thesen/22These2.mp3",
-    "source/thesen/22These3.mp3",
-    "source/thesen/23These1.mp3",
-    "source/thesen/27These1.mp3",
-    "source/thesen/27These2.mp3",
-    "source/thesen/27These3.mp3",
-    "source/thesen/28These1.mp3",
-    "source/thesen/28These2.mp3",
-    "source/thesen/28These3.mp3",
-    "source/thesen/31These1.mp3",
-    "source/thesen/31These2.mp3",
-    "source/thesen/31These3.mp3",
-    "source/thesen/35These1.mp3",
-    "source/thesen/35These2.mp3",
-    "source/thesen/35These3.mp3",
-    "source/thesen/37These1.mp3",
-    "source/thesen/37These2.mp3",
-    "source/thesen/37These3.mp3",
-    "source/thesen/40These1.mp3",
-    "source/thesen/40These2.mp3",
-    "source/thesen/40These3.mp3"
+    "https://sodnor.github.io/fakten/source/thesen/14These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/14These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/14These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/15These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/15These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/15These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/16These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/16These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/16These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/17These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/17These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/17These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/18These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/18These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/18These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/20These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/20These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/20These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/21These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/21These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/21These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/22These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/22These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/22These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/23These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/27These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/27These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/27These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/28These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/28These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/28These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/31These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/31These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/31These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/35These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/35These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/35These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/37These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/37These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/37These3.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/40These1.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/40These2.mp3",
+    "https://sodnor.github.io/fakten/source/thesen/40These3.mp3"
 ];
 
 var txtLoad = [
@@ -161,46 +147,46 @@ var phThese = [
 ];
 
 var srcJaNein = [
-    "source/janein/15Ja1.mp3",
-    "source/janein/15Ja2.mp3",
-    "source/janein/15Ja3.mp3",
-    "source/janein/16Ja1.mp3",
-    "source/janein/16Ja2.mp3",
-    "source/janein/16Ja3.mp3",
-    "source/janein/17Ja1.mp3",
-    "source/janein/17Ja2.mp3",
-    "source/janein/17Ja3.mp3",
-    "source/janein/18Ja1.mp3",
-    "source/janein/18Ja2.mp3",
-    "source/janein/18Ja3.mp3",
-    "source/janein/20Ja1.mp3",
-    "source/janein/20Ja2.mp3",
-    "source/janein/20Ja3.mp3",
-    "source/janein/21Ja1.mp3",
-    "source/janein/21Ja2.mp3",
-    "source/janein/21Ja3.mp3",
-    "source/janein/22Ja1.mp3",
-    "source/janein/22Ja2.mp3",
-    "source/janein/22Ja3.mp3",
-    "source/janein/27Ja1.mp3",
-    "source/janein/27Ja2.mp3",
-    "source/janein/27Ja3.mp3",
-    "source/janein/28Ja1.mp3",
-    "source/janein/28Ja2.mp3",
-    "source/janein/28Ja3.mp3",
-    "source/janein/31Ja1.mp3",
-    "source/janein/31Ja2.mp3",
-    "source/janein/31Ja3.mp3",
-    "source/janein/35Ja1.mp3",
-    "source/janein/35Ja1z.mp3",
-    "source/janein/35Ja2.mp3",
-    "source/janein/35Ja3.mp3",
-    "source/janein/37Ja1.mp3",
-    "source/janein/37Ja2.mp3",
-    "source/janein/37Ja3.mp3",
-    "source/janein/40Ja1.mp3",
-    "source/janein/40Ja2.mp3",
-    "source/janein/40Ja3.mp3"
+    "https://sodnor.github.io/fakten/source/janein/15Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/15Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/15Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/16Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/16Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/16Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/17Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/17Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/17Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/18Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/18Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/18Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/20Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/20Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/20Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/21Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/21Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/21Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/22Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/22Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/22Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/27Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/27Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/27Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/28Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/28Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/28Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/31Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/31Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/31Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/35Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/35Ja1z.mp3",
+    "https://sodnor.github.io/fakten/source/janein/35Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/35Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/37Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/37Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/37Ja3.mp3",
+    "https://sodnor.github.io/fakten/source/janein/40Ja1.mp3",
+    "https://sodnor.github.io/fakten/source/janein/40Ja2.mp3",
+    "https://sodnor.github.io/fakten/source/janein/40Ja3.mp3"
 ];
 
 var emojis = [
